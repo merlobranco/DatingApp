1e3d21d999b3c814148fae1b22d3df679ed65033
Brais Cidras <braisomary@gmail.com>
2021-07-29 14:01:42 +0200

    Using DTOs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,8 +2,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -16,14 +18,17 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password) 
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO) 
         {
+            if (await UserExists(registerDTO.Username)) 
+                return BadRequest("Username is taken"); 
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDTO.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -32,5 +37,8 @@ namespace API.Controllers
             return user;
         }
 
+        private async Task<bool> UserExists(string username) {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+        }
     }
 }
