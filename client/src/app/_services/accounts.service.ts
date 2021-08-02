84d39ea58170514810db338609d0b74d3ffab67f
Brais Cidras <braisomary@gmail.com>
2021-08-02 16:36:23 +0200

    Persisting the login

--- client/src/app/_services/accounts.service.ts
+++ client/src/app/_services/accounts.service.ts
@@ -1,15 +1,41 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountsService {
-  baseUrl = 'https://localhost:5001/api/'
+  baseUrl = 'https://localhost:5001/api/';
+
+  // Creating an observable for storing our user
+  // A replaysubject is an buffer and everytime a subscriber subscribes to this observable
+  // it will emit the last value inside it, or however values we want to emit inside it
+  private currentUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient) { }
 
   login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model);
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: User) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUserSource.next(user);
+        }
+      })
+      )
+    }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
+  }
+     
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUserSource.next(null);
   }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -14,19 +14,29 @@ export class NavComponent implements OnInit {
   constructor(private accountService: AccountsService) { }
 
   ngOnInit(): void {
+    this.getCurrentUser();
   }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
       this.loggedIn = true;
-    },
-    error => {
+    }, error => {
       console.log(error);
     });
   }
 
   logout() {
+    this.accountService.logout();
     this.loggedIn = false;
+  }
+
+  getCurrentUser() {
+    this.accountService.currentUser$.subscribe(user => {
+      this.loggedIn = !!user;
+    }, error => {
+        console.log(error);
+      }
+    );
   }
 }
